3c6dde1b14199356c3388fd657f7a381b4434dbd
mtomljanovic1 <markot@windowslive.com>
2023-07-07 23:55:53 +0200

    feat: create repository

---
+++ tracking-tool/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { Button } from "primereact/button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button label="marko" className="p-button-secondary" />
+    </div>
+  );
+}
+
+export default App;
